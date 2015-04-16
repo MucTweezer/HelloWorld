c56849b2731bc087c393b1027a91afc2cb84ec0c
MucTweezer <matt.s.brockman@gmail.com>
2015-04-16 16:05:18 -0600

    Some exposition

--- mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
+++ mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
@@ -1,3 +1,10 @@
+/*
+ * Testing out the saving, deleting, and opening of a single file.
+ * I have opted to use a bunch of static methods and filename to test out a system with only a single file.
+ * Presumably, I would use this for persistent non-temporary data storage for an app.
+ * All data would be stored in the single file, which would be recreated upon each save (not appended).
+ */
+ 
 package me.muctweezer.helloworld
 
 import java.io.*;
