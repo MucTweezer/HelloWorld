0603394c31c4d8869bfb0360716a4cf45a04d67a
MucTweezer <matt.s.brockman@gmail.com>
2015-04-16 14:39:09 -0600

    Initial
    Skeleton for a class to play with saving, opening, and deleting a file.

---
+++ mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
@@ -0,0 +1,17 @@
+package me.muctweezer.helloworld
+
+import java.io.*;
+
+private static final String FILENAME = "temp.txt"
+
+public class FileManagement {
+
+    public static void saveFile() {
+    }
+    
+    public static void openFile() {
+    }
+    
+    public static void deleteFile() {
+    }
+}
