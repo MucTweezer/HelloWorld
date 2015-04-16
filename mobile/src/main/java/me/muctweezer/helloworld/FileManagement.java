08f8d9892d3c5b6530a380702434da97836c6318
MucTweezer <matt.s.brockman@gmail.com>
2015-04-16 15:48:52 -0600

    saveFile function plus other stuff
    Messy commits are my jam, yo.

--- mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
+++ mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
@@ -2,16 +2,33 @@ package me.muctweezer.helloworld
 
 import java.io.*;
 
-private static final String FILENAME = "temp.txt"
+private static final String FILENAME = "temp.txt";
 
 public class FileManagement {
 
     public static void saveFile() {
+        String data = "Oh my gosh.";
+        FileOutputStream outputStream;
+        
+        try {
+            if fileExists() {
+                deleteFile();
+            }
+            
+            outputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            outputStream.write(data.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     public static void openFile() {
     }
     
     public static void deleteFile() {
+    }
+    
+    private boolean fileExists() {
     }
 }
