9288948530e95adb89f8e079b0f4bae4b52da142
Matt Brockman <matt.s.brockman@gmail.com>
2015-04-16 17:02:30 -0600

    Completely Redone
    Changed the handling of the File and the re-thought the idea of making
    this static.

--- mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
+++ mobile/src/main/java/me/muctweezer/helloworld/FileManagement.java
@@ -1,27 +1,35 @@
 /*
  * Testing out the saving, deleting, and opening of a single file.
  * I have opted to use a bunch of static methods and filename to test out a system with only a single file.
+ * (Perhaps using static methods was not going to work after all. I guess I'm going to make this more of a helper class.
+ * MainActivity is going to have to create an instance of this on startup and keep that instance.
  * Presumably, I would use this for persistent non-temporary data storage for an app.
  * All data would be stored in the single file, which would be recreated upon each save (not appended).
  */
  
 package me.muctweezer.helloworld;
 
+import android.content.Context;
 import java.io.*;
 
-private static final String FILENAME = "temp.txt";
-
-private String myData = "oh my gosh";
-
 public class FileManagement {
+    private final String FILENAME = "temp.txt";
+    private String myData = "oh my gosh";
+    private Context myContext;
+    private File myFile;
 
-    public static void saveFile() {
+    public FileManagement(Context tempContext) {
+        myContext = tempContext;
+        myFile = new File(myContext.getFilesDir(), FILENAME);
+    }
+
+    public void saveFile() {
         FileOutputStream outputStream;
         
         try {
             deleteFile();
             
-            outputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            outputStream = myContext.openFileOutput(FILENAME, Context.MODE_PRIVATE);
             outputStream.write(myData.getBytes());
             outputStream.close();
         } catch (Exception e) {
@@ -30,18 +38,18 @@ public class FileManagement {
     }
     
     private File getFile() {
-        File myFile = new File(context.getFilesDir(), FILENAME);
+        File myFile = new File(myContext.getFilesDir(), FILENAME);
         return myFile;
     }
     
-    public static void deleteFile() {
-        if fileExists() {
-            getFile().delete();
+    public void deleteFile() {
+        if(fileExists()) {
+            myFile.delete();
         }
     }
     
     private boolean fileExists() {
-        return getFile().exists();
+        return myFile.exists();
     }
     
     public String getData() {
